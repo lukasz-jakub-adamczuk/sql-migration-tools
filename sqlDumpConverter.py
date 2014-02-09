3e7eb170400b97b9b10af7129e83f6109234667c
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2014-02-09 22:59:13 +0100

    migration Converter  updated

--- sqlDumpConverter.py
+++ sqlDumpConverter.py
@@ -65,129 +65,162 @@ class OrderedDictYAMLLoader(yaml.Loader):
 
 
 def mapFields(oldFields, newFields, config, values):
-	fields = []
-	for new in newFields:
-		column = config['fields'][new]['column']
-		if isinstance(column, list):
-			value = ''
-			for item in column:
-				if item in oldFields:
-					value += str(values[oldFields[item]])[1:-1]
-				else:
-					value += str(item)
-			fields.append("'" + value + "'")
-		elif column == None:
-			# column is None
-			value = ''
-			default = config['fields'][new]['default']
-			if default == None:
-				value = "NULL"
-			elif isinstance(default, str) and default[0] == '<':
-				# special function
-				splits = default[1:-1].split(':')
-				action = splits[0]
-				col = splits[1]
-				if action == 'slug':
-					value = "'" + str(slugify(values[oldFields[col]][1:-1])) + "'"
-				if action == 'categories':
-					key = values[oldFields[col]][1:-1].replace('_', '-').replace('-1', '').replace('--', '-')
-					key = key.replace('fantasy-2', 'fantasy').replace('livealive', 'live-a-live').replace('3582', '358-2').replace('x2', 'x-2').replace('recoded', 're-coded')
-					value = "'" + str(categories[key]) + "'"
-			else:
-				value = default
-			# print str(new) + ' -> ' + str(column) + ' -> ... -> ' + str(default)
-			fields.append(value)
-		else:
-			if isinstance(column, str) and column[0] == '<':
-				splits = column[1:-1].split(':')
-				action = splits[0]
-				col = splits[1]
-				if action == 'map':
-					map = config['fields'][new]['map']
+    fields = []
+    for new in newFields:
+        column = config['fields'][new]['column']
+        if isinstance(column, list):
+            value = ''
+            for item in column:
+                if item in oldFields:
+                    value += str(values[oldFields[item]])[1:-1]
+                else:
+                    value += str(item)
+            fields.append("'" + value + "'")
+        elif column == None:
+            # column is None
+            value = ''
+            default = config['fields'][new]['default']
+            if default == None:
+                value = "NULL"
+            elif isinstance(default, str) and default[0] == '<':
+                # special function
+                splits = default[1:-1].split(':')
+                action = splits[0]
+                col = splits[1]
+                if action == 'slug':
+                    value = "'" + str(slugify(values[oldFields[col]][1:-1])) + "'"
+                if action == 'categories':
+                    key = values[oldFields[col]][1:-1]
+                    if 'replace' in config['fields'][new]:
+                        replace = config['fields'][new]['replace']
+                        for k,v in replace.items():
+                            key = key.replace(k, v)
+                    value = "'" + str(categories[key]) + "'"
+            else:
+                value = default
+            # print str(new) + ' -> ' + str(column) + ' -> ... -> ' + str(default)
+            fields.append(value)
+        else:
+            if isinstance(column, str) and column[0] == '<':
+                splits = column[1:-1].split(':')
+                action = splits[0]
+                col = splits[1]
+                if action == 'map':
+                    map = config['fields'][new]['map']
 
-					key = values[oldFields[col]]
-					if len(key) > 0 and key[0] == "'":
-						key = key[1:-1]
-					else:
-						key = key
-					
-					if key in map:
-						value = "'" + str(map[key]) + "'"
-					else:
-						print 'Key problem:'
-						value = "'" + config['fields'][new][default] + "'"
-					fields.append(value)
-			else:
-				key = oldFields[column]
-				# print column
-				# print key
-				# print values[key]
-				# print str(new) + ' -> ' + str(column) + ' -> ' + str(key) + ' -> ' + values[key]
-				fields.append(values[key])
+                    key = values[oldFields[col]]
+                    if len(key) > 0 and key[0] == "'":
+                        key = key[1:-1]
+                    else:
+                        key = key
+                    
+                    if key in map:
+                        value = "'" + str(map[key]) + "'"
+                    else:
+                        print 'Key problem:'
+                        value = "'" + config['fields'][new][default] + "'"
+                    fields.append(value)
+            else:
+                key = oldFields[column]
+                # print column
+                # print key
+                # print values[key]
+                # print str(new) + ' -> ' + str(column) + ' -> ' + str(key) + ' -> ' + values[key]
+                fields.append(values[key])
 
-	q = ''
-	for field in fields:
-		q += str(field) + ','
-	q = q[0:-1]
-	return q
+    q = ''
+    for field in fields:
+        q += str(field) + ','
+    q = q[0:-1]
+    return q
 
 
 def splitValues(values):
-	# print values
+    # print values
 
-	parts = values.split(',')
+    parts = values.split(',')
 
-	fields = []
-	tmp = False
+    fields = []
+    tmp = False
 
-	for chars in parts:
-		if tmp:
-			tmp += ',' + chars
-		else:
-			tmp = chars
+    for chars in parts:
+        if tmp:
+            tmp += ',' + chars
+        else:
+            tmp = chars
 
-		# query begin and first value
-		if len(tmp) > 6 and tmp[0:6] == 'INSERT':
-			pos = tmp.index('(')
-			fields.append(tmp[pos+1:])
-			tmp = False
-		else:
-			if len(tmp) > 1 and tmp[-2:] == ');':
-				pos = tmp.index(')')
-				fields.append(tmp[0:pos])
-				tmp = False
-			else:
-				# fields without comma
-				# if tmp[0] == "'" and tmp[-1] == "'" and tmp[-2] != "\\":
-				if tmp[0] == "'" and tmp[-1] == "'":
-					if tmp[-2] != "\\" or (tmp[-3:-1] == "\\\\" and tmp[-4] != "\\") or (tmp[-5:-1] == "\\\\\\\\" and tmp[-6] != "\\"):
-						# print tmp[-5:-1]
-						fields.append(tmp)
-						# fields.append(tmp[-5:])
-						tmp = False
-				else:
-					if tmp.isdigit():
-						fields.append(tmp)
-						tmp = False
-	# print fields
-	return fields
+        # query begin and first value
+        if len(tmp) > 6 and tmp[0:6] == 'INSERT':
+            pos = tmp.index('(')
+            fields.append(tmp[pos+1:])
+            tmp = False
+        else:
+            if len(tmp) > 1 and tmp[-2:] == ');':
+                pos = tmp.index(')')
+                fields.append(tmp[0:pos])
+                tmp = False
+            else:
+                # fields without comma
+                # if tmp[0] == "'" and tmp[-1] == "'" and tmp[-2] != "\\":
+                if tmp[0] == "'" and tmp[-1] == "'":
+                    if tmp[-2] != "\\" or (tmp[-3:-1] == "\\\\" and tmp[-4] != "\\") or (tmp[-5:-1] == "\\\\\\\\" and tmp[-6] != "\\"):
+                        # print tmp[-5:-1]
+                        fields.append(tmp)
+                        # fields.append(tmp[-5:])
+                        tmp = False
+                else:
+                    if tmp.isdigit():
+                        fields.append(tmp)
+                        tmp = False
+    # print fields
+    return fields
+
+def parseArgs(args):
+    options = {}
+    for param in args:
+        if len(param) > 2:
+            if param[0:2] == '--':
+                pos = param.index('=')
+                key = param[2:pos]
+                val = param[pos+1:]
+                if val == 'false' or val == 'true':
+                    val = bool()
+                options[key] = val
+    return options
+
+# options
+# --table
+# --output
 
 
 # main code
 if len(sys.argv) < 1:
-	print 'Not enough arguments'
-	sys.exit(1)
+    print 'Not enough arguments'
+    sys.exit(1)
 else:
-	source = sys.argv[1]
+    options = parseArgs(sys.argv)
 
+# print sys.argv
+# print options
 
-input = open('category.json')
+if options['table']:
+    directory = options['table']
+else:
+    sys.exit(1)
+
+output = './'
+if 'output' in options:
+    output = options['output']
+    if output[-1] != '/':
+        output += '/'
+
+input = open(output + 'category.json')
 lines = input.read()
 categories = json.loads(lines)
 input.close()
 
 
-config = 'tables/' + source + '/config.yml'
+config = output + 'tables/' + directory + '/config.yml'
 
 input = open(config)
 mapping = yaml.load(input, Loader=OrderedDictYAMLLoader)
@@ -196,7 +229,7 @@ input.close()
 table = mapping['table']
 
 
-filename = 'tables/' + source + '/source.sql'
+filename = 'tables/' + directory + '/source.sql'
 
 input = open(filename)
 lines = input.read().splitlines()
@@ -207,10 +240,10 @@ oldFields = {}
 i = 0
 # getting old table fields list
 for line in lines:
-	if len(line) > 3 and line[2] == '`':
-		pos = line.index('`', 3)
-		oldFields[line[3:pos]] = i
-		i += 1
+    if len(line) > 3 and line[2] == '`':
+        pos = line.index('`', 3)
+        oldFields[line[3:pos]] = i
+        i += 1
 
 
 newFields = mapping['fields'].keys()
@@ -218,7 +251,7 @@ newFields = mapping['fields'].keys()
 # table fields to final sql
 tableFields = ''
 for field in mapping['fields'].keys():
-	tableFields += '`' + field + '`, '
+    tableFields += '`' + field + '`, '
 tableFields = tableFields[0:-2] 
 
 
@@ -236,16 +269,16 @@ values_cnt = 0
 
 i = 0
 for line in lines:
-	if line[0:6] == 'INSERT':
-		queries.append(line.split('),('))
-		i += 1
+    if line[0:6] == 'INSERT':
+        queries.append(line.split('),('))
+        i += 1
 
 i = 0
 for query in queries:
-	j = 0
-	for value in query:
-		values.append(splitValues(value))
-		i += 1
+    j = 0
+    for value in query:
+        values.append(splitValues(value))
+        i += 1
 
 
 
@@ -254,28 +287,19 @@ sql += "TRUNCATE TABLE `" + table + "`;\n";
 
 i = 0
 for query in queries:
-	sql += 'INSERT INTO `' + table + '`'
-	sql += ' (' + tableFields + ')'
-	sql += ' VALUES '
-	for value in query:
-		sql += '(' + mapFields(oldFields, newFields, mapping, values[i]) + ')'
-		sql += ','
-		i += 1
-	sql = sql[:-1]
-	sql += ";\n"
+    sql += 'INSERT INTO `' + table + '`'
+    sql += ' (' + tableFields + ')'
+    sql += ' VALUES '
+    for value in query:
+        sql += '(' + mapFields(oldFields, newFields, mapping, values[i]) + ')'
+        sql += ','
+        i += 1
+    sql = sql[:-1]
+    sql += ";\n"
 
 
-output = open('tables/' +  source + '/' + 'output.sql', 'w')
+output = open('tables/' +  directory + '/' + 'output.sql', 'w')
 output.write(sql)
 output.close()
 
-# print len(queries)
-# print len(values)
-
-# print values[2812][0]
-# print values[2812][1]
-# print values[2812][2]
-# print values[2812][3]
-# print values[2812][4]
-# print values[2812][5]
 
